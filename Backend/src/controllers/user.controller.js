3cfbd03dd3055d85abf975c638b61519c4f20e96
Karansinh Jadav <karanjadav1772004@gmail.com>
2026-06-15 19:10:24 +0530

    fix cookis problem

--- Backend/src/controllers/user.controller.js
+++ Backend/src/controllers/user.controller.js
@@ -2,70 +2,78 @@ import userModel from '../models/user.model.js'
 import userService from '../services/user.service.js'
 import redisClient from '../services/redis.service.js'
 
-export const createUserController = async (req,res)=>{
+export const createUserController = async (req, res) => {
 
     try {
         const user = await userService.createUser(req.body);
         const token = user.generateJWT();
-        res.cookie("token",token);
+        res.cookie("token", token, {
+            httpOnly: true,
+            secure: true,
+            sameSite: "none"
+        });
         delete user._doc.password;
-        res.status(201).json({user,token});
+        res.status(201).json({ user, token });
     }
-    catch(err){
+    catch (err) {
         res.status(400).send(err.message);
     }
 }
-export const loginController = async (req,res)=>{
+export const loginController = async (req, res) => {
 
     try {
-        const {email , password} = req.body;
+        const { email, password } = req.body;
 
-        const user = await userModel.findOne({email}).select("+password ");
+        const user = await userModel.findOne({ email }).select("+password ");
 
-        if(!user){
+        if (!user) {
             return res.status(401).json({
                 errors: "Invalid Credentials"
             })
         }
         const isMatchPassword = await user.isValidPassword(password);
-        if(!isMatchPassword){
+        if (!isMatchPassword) {
             return res.status(401).json({
                 errors: "Invalid Credentials"
             })
         }
         const token = await user.generateJWT()
 
-        res.cookie("token",token)
+        res.cookie("token", token, {
+            httpOnly: true,
+            secure: true,
+            sameSite: "none"
+        })
         delete user._doc.password;
         res.status(201).json({
-        message:"User logged in successfully",
-        user,token,
-    })
+            message: "User logged in successfully",
+            user, token,
+        })
     }
-    catch(err){
+    catch (err) {
         res.status(400).send(err.message);
     }
 }
-export const profileController = async (req,res)=>{
+export const profileController = async (req, res) => {
 
     try {
         const email = req.user.email;
-        const user = await userModel.findOne({email}).select("+password ");
+        const user = await userModel.findOne({ email }).select("+password ");
         res.status(201).json({
             user,
-    })
+        })
     }
-    catch(err){
+    catch (err) {
         res.status(400).send(err.message);
     }
 }
-export const logoutController = async (req,res)=>{
+export const logoutController = async (req, res) => {
 
     try {
         const token = req.cookies?.token ||
-        req.headers.authorization?.split(" ")[1];
+            req.headers.authorization?.split(" ")[1];
 
-        await redisClient.set(token,'logout', 'EX' , 60*60*24);
+        await redisClient.set(token, 'logout', 'EX', 60 * 60 * 24);
 
         res.clearCookie("token");
 
@@ -73,23 +81,23 @@ export const logoutController = async (req,res)=>{
             message: 'Logged out successfully'
         })
     }
-    catch(err){
+    catch (err) {
         res.status(400).send(err.message);
     }
 }
 
-export const getAllUsersController = async(req,res)=>{
+export const getAllUsersController = async (req, res) => {
     try {
         const loggedInUser = await userModel.findOne({
-            email:req.user.email
+            email: req.user.email
         })
-        const allUsers = await userService.getAllUsers({ userId:loggedInUser._id }); 
+        const allUsers = await userService.getAllUsers({ userId: loggedInUser._id });
 
         res.status(200).json({
             allUsers
         })
     }
-    catch(err){
+    catch (err) {
         res.status(400).send(err.message);
     }
 }
