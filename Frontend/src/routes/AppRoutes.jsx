358153e17d6b04eed4f0ad30b076b03c0cb7f95f
Karansinh Jadav <karanjadav1772004@gmail.com>
2026-06-15 18:56:07 +0530

    fix refresh problem

--- Frontend/src/routes/AppRoutes.jsx
+++ Frontend/src/routes/AppRoutes.jsx
@@ -13,7 +13,11 @@ const AppRoutes = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Home />} />
+        <Route path='/' element={
+          <ProtectedRoute>
+            <Home />
+          </ProtectedRoute>
+          } />
         <Route path='/*' element={<NotFound />} />
         <Route path='/login' element={<Login />} />
         <Route path='/Register' element={<Register />} />
